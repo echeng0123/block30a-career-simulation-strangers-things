221fdd1d158a07037d6201b146221ba5fb2d2e3f
Emily Cheng <echeng0123@gmail.com>
2023-08-22 02:40:25 -0400

    ok final one for real this time

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,9 +1,7 @@
 // This component handles user logoff.
 
 import { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-// import { logout } from "../redux/authSlice";
 
 export default function Logout() {
 	// get current user status
@@ -11,5 +9,5 @@ export default function Logout() {
 	useEffect(() => {
 		navigate(0);
 		navigate("/home");
-	}, []);
+	}, [navigate]);
 }
