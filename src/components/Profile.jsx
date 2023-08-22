373f69da183de37626352e48fb0cbe396c5181e9
Emily Cheng <echeng0123@gmail.com>
2023-08-21 22:21:05 -0400

    working messages panel

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,12 @@
 // This component renders the logged in user profile.
-import { fetchAllPosts, fetchUserProfile } from "../API/STindex";
+import { fetchUserProfile } from "../API/STindex";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import EditPost from "./EditPost";
 import DeletePost from "./DeletePost";
+import MessagePanel from "./MessagePanel";
 
 export default function Profile() {
 	const [userPosts, setUserPosts] = useState([]);
@@ -48,7 +49,7 @@ export default function Profile() {
 		  )
 		: userPosts;
 
-	const userMessagesToDisplay = searchParam
+	const userMessagesToDisplay = searchMessageParam
 		? userMessages.filter(
 				(message) =>
 					message.post.title.toLowerCase().includes(searchMessageParam) ||
@@ -62,75 +63,67 @@ export default function Profile() {
 				<h1>Welcome, {profileUsername}.</h1>
 				<h3>Your user id is: {userId}</h3>
 			</div>
-			<div>
-				<label>
-					Search your active posts:{" "}
-					<input
-						type="text"
-						placeholder="Search title or description"
-						onChange={(event) =>
-							setSearchParam(event.target.value.toLowerCase())
-						}
-					/>
-				</label>
-			</div>
-			<div>
-				<label>
-					Search your sent messages:{" "}
-					<input
-						type="text"
-						placeholder="Search title or description"
-						onChange={(event) =>
-							setSearchMessageParam(event.target.value.toLowerCase())
-						}
-					/>
-				</label>
-			</div>
-			<div id="your-posts-header">
-				<h2>Your Active Listings: </h2>
-			</div>
-			<div id="all-posts-gallery">
-				{error && <p>{error}</p>}
-				{userPostsToDisplay.map((post) => {
-					const postIdA = post._id;
-					if (post.active) {
-						return (
-							<>
-								<div id="each-post">
-									<h3>{post.title}</h3>
-									<h5>Seller: {post.author.username}</h5>
-									<h5 id="post-price">Price: {post.price}</h5>
-									<h5>Location: {post.location}</h5>
-									<h5>Delivery Available: {post.willDeliver ? "Yes" : "No"}</h5>
-									<p id="post-description">{post.description}</p>
-									<div>
-										<EditPost postId={postIdA} />
-										<DeletePost postId={postIdA} />
-									</div>
-								</div>
-							</>
-						);
-					} else {
-						return <></>;
-					}
-				})}
-			</div>
-			<div id="messages-panel">
-				{error && <p>{error}</p>}
-				{userMessagesToDisplay.map((message) => {
-					const messageTitle = message.post.title;
-					// const messagePostId = message.post._id;
-					const messageContent = message.content;
-
-					return (
-						<>
-							<div id="each-message">
-								<h3>Re: Post: {messageTitle}</h3>
-								<p id="message-description">{messageContent}</p>
-							</div>
-						</>
-					);
-				})}
+			<div id="profile-content">
+				<div id="messages-section-profile">
+					<label>
+						Search your sent messages:{" "}
+						<input
+							type="text"
+							placeholder="Search title or description"
+							onChange={(event) =>
+								setSearchMessageParam(event.target.value.toLowerCase())
+							}
+						/>
+					</label>
+					<div>
+						<MessagePanel />
+					</div>
+				</div>
+				<div id="posts-section-profile">
+					<div id="your-posts-header">
+						<h2>Your Active Listings: </h2>
+					</div>
+					<div>
+						<label>
+							Search your active posts:{" "}
+							<input
+								type="text"
+								placeholder="Search title or description"
+								onChange={(event) =>
+									setSearchParam(event.target.value.toLowerCase())
+								}
+							/>
+						</label>
+					</div>
+					<div id="all-posts-gallery">
+						{error && <p>{error}</p>}
+						{userPostsToDisplay.map((post) => {
+							const postIdA = post._id;
+							if (post.active) {
+								return (
+									<>
+										<div id="each-post">
+											<h3>{post.title}</h3>
+											<h5>Seller: {post.author.username}</h5>
+											<h5 id="post-price">Price: {post.price}</h5>
+											<h5>Location: {post.location}</h5>
+											<h5>
+												Delivery Available: {post.willDeliver ? "Yes" : "No"}
+											</h5>
+											<p id="post-description">{post.description}</p>
+											<div>
+												<EditPost postId={postIdA} />
+												<DeletePost postId={postIdA} />
+											</div>
+										</div>
+									</>
+								);
+							} else {
+								return <></>;
+							}
+						})}
+					</div>
+				</div>
 			</div>
 		</>
 	);
